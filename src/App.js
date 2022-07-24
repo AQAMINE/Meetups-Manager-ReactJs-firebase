7a0a6a078b0379409fad493a83135391c43cd081
amine aqebli <aqebli36@gmail.com>
2022-07-24 21:21:04 +0100

    Add style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
-
+import MainNavigation from './components/layout/MainNavigation';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
 
 function App() {
   return <div>
+    <MainNavigation/>
     <Switch>
-      <Route path='/' exact>
+      <Route path='/' exact >
         <AllMeetupsPage/>
       </Route>
       <Route path='/new-meetup'>
