71464ba39ee3cbdbda6bd8f5f69b5df48c429748
amine aqebli <aqebli36@gmail.com>
2022-07-31 03:26:38 +0100

    Show Favorites Item

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,21 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage () {
+    const faveoritesCnx = useContext(FavoritesContext);
+
+    let content;
+    if(faveoritesCnx.totalFavorites === 0){
+        content =  <p>You got no favorite yet. Start adding some?</p>
+    }else{
+        content = <MeetupList meetups={faveoritesCnx.favorites} />
+    }
     return (
-        <div>Favorits</div>
+        <section>
+            <h1>My Favorites</h1>
+            {content}
+        </section>
     );
 }
 export default FavoritesPage;
