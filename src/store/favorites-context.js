3139d28dab55909d3d5e6b03293987d31842633c
amine aqebli <aqebli36@gmail.com>
2022-07-31 02:50:21 +0100

    meetup context setup

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -2,18 +2,45 @@ import { createContext, useState } from "react";
 
 const FavoritesContext = createContext({
     favorites: [],
-    totalFavorites: 0
+    totalFavorites: 0,
+    addFavorite: (favoriteItem) => {},
+    removeFavorite: (meetupId) => {},
+    itemIsFavorite: (meetupId) => {}
 });
 
-function FavoritesContextProvider (props) {
+export function FavoritesContextProvider (props) {
     const [userFavorites, setUserFavorites] = useState([]);
+
+    function addFavoritesHandler (favoritMeetup) {
+        setUserFavorites( (prevUserFavorits) => {
+            return prevUserFavorits.concat(favoritMeetup);
+        });
+    };
+    function removeFavoritesHandler (meetupId) {
+        setUserFavorites(prevUserFavorits => {
+            //collect without the item contain this id 
+            return prevUserFavorits.filter(meetup => meetup.id !== meetupId );
+        });
+    };
+    function itemIsFavoritesHandler (meetupId) {
+        //Get The item whene the meetupId is the some 
+        return userFavorites.some(meetup => meetup.id === meetupId);
+    };
 
     const context = {
         favorites: userFavorites,
         totalFavorites: userFavorites.length,
+        addFavorite: addFavoritesHandler,
+        removeFavorite: removeFavoritesHandler,
+        itemIsFavorite: itemIsFavoritesHandler
     }; 
 
-    return <FavoritesContext.Provider value={context}>
-        {props.children}
-    </FavoritesContext.Provider>
+    return (
+        <FavoritesContext.Provider value={context}>
+            {props.children}
+        </FavoritesContext.Provider>
+    );
+    
 }
+
+export default FavoritesContext;
